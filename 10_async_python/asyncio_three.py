9dbe092b9ebb31d54a5df7cdeb1580fe38bb4310
Ayush0115D <dhakreayush578@gmail.com>
2025-12-19 23:30:08 +0530

    mixing thread with asyncio

---
+++ 10_async_python/asyncio_three.py
@@ -0,0 +1,14 @@
+import asyncio
+import time
+from concurrent.futures import ThreadPoolExecutor
+def check_stock(item):
+    print(f"Checking {item} in store.. ")
+    time.sleep(3) #blocking operation
+    return f"{item} is in stock!"
+async def main():
+    loop=asyncio.get_running_loop()
+    with ThreadPoolExecutor() as pool:
+        result = await loop.run_in_executor(pool,
+        check_stock,"masala chai")
+        print(result) 
+asyncio.run(main())
